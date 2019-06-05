3c95078ee8ec784982c526ba2b7a80b818f6f750
liyili <732193462@qq.com>
2019-06-05 16:11:48 +0800

    feat:init 2 java files

---
+++ Dog.java
@@ -0,0 +1,14 @@
+public class Dog{
+    // String breed;
+    // int age;
+    // String color;
+    // void barking(){}
+    // void hungry(){}
+    // void sleeping(){}
+    public Dog(String name){
+        System.out.println("Passed name is:"+name);
+    }
+    public static void main(String [] args){
+        Dog yinu=new Dog("puppy");
+    }
+}
